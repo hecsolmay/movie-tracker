bdded1119bae26afc0beca20dbd1b224daba93a2
hecsolmay <hecsolmay@gmail.com>
2024-10-17 00:26:08 -0600

    ad config and mock constants

---
+++ src/constants/config.ts
@@ -0,0 +1,1 @@
+export const OMDB_API_KEY = import.meta.env.OMDB_API_KEY as string
---
+++ src/constants/mock.ts
@@ -0,0 +1,30 @@
+export const movies = [
+  {
+    id: 1,
+    title: 'Inception',
+    releaseDate: '2010',
+    poster: 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+    watched: false
+  },
+  {
+    id: 2,
+    title: 'The Shawshank Redemption',
+    releaseDate: '1994',
+    poster: 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+    watched: true
+  },
+  {
+    id: 3,
+    title: 'The Godfather',
+    releaseDate: '1972',
+    poster: 'https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+    watched: false
+  },
+  {
+    id: 4,
+    title: 'Pulp Fiction',
+    releaseDate: '1994',
+    poster: 'https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg',
+    watched: true
+  }
+]
