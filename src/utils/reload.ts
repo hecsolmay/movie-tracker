4152d27792b4aadc763fe5eafbe5329dc9318dd3
hecsolmay <hecsolmay@gmail.com>
2024-10-17 03:08:20 -0600

    add new reload function

--- src/utils/reload.ts
+++ src/utils/reload.ts
@@ -1,4 +1,5 @@
-import { $$ } from '@lib/dom'
+import { $, $$ } from '@lib/dom'
+import type { Movie } from '../types/movies'
 
 const tabs = $$<HTMLButtonElement>('.tab')
 const movieCards = $$('.movie-card')
@@ -27,4 +28,57 @@ export function getActiveFilter () {
 export function reloadFilteredMovies () {
   const activeTab = getActiveFilter()
   loadFilteredMovies(activeTab)
+}
+
+export function addNewMovieToList (movie: Movie) {
+  const movieGrid = $<HTMLDivElement>('.movie-grid')
+  const movieCardTemplate = $<HTMLTemplateElement>('#movie-template')
+
+  if (movieCardTemplate === null || movieGrid === null) return
+
+  const clone = document.importNode(movieCardTemplate.content, true)
+  // Configurar los datos dinámicos
+  const movieCard = clone.querySelector('.movie-card')
+  const img = clone.querySelector('img')
+  const title = clone.querySelector('h3')
+  const releaseYear = clone.querySelector('p')
+  const watchBtn = clone.querySelector('.watch-btn')
+
+  if (
+    movieCard === null ||
+    img === null ||
+    title === null ||
+    releaseYear === null ||
+    watchBtn === null
+  ) {
+    return
+  }
+
+  movieCard.setAttribute('data-status', movie.watched ? 'watched' : 'unwatched')
+
+  img.src = movie.poster
+  img.alt = `${movie.title} poster`
+
+  title.textContent = movie.title
+
+  releaseYear.textContent = movie.releaseYear
+
+  watchBtn.textContent = movie.watched ? 'Unwatch' : 'Watch'
+
+  // Añadir al DOM
+  movieGrid.insertBefore(clone, movieGrid.firstChild)
+  const watchBtns = $$<HTMLButtonElement>('.watch-btn')
+
+  watchBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      // @ts-expect-error ts-expect-error
+      const card = e.target.closest('.movie-card')
+      const currentStatus = card.dataset.status
+      const newStatus = currentStatus === 'watched' ? 'unwatched' : 'watched'
+      card.dataset.status = newStatus
+      btn.textContent = newStatus === 'watched' ? 'Unwatch' : 'Watch'
+      reloadFilteredMovies()
+      // Here you would typically update the movie's watched status in your data store
+    })
+  })
 }
