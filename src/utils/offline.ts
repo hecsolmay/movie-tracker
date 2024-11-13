c3ebcbaeed56f0ff7e09a92d6ff9012567ac2617
hecsolmay <hecsolmay@gmail.com>
2024-11-13 12:50:02 -0600

    syncLocalDbWithServer when content is loaded

--- src/utils/offline.ts
+++ src/utils/offline.ts
@@ -2,6 +2,14 @@ import { syncLocalDBWithServer } from './sync'
 
 console.log('Load offline utilities')
 
+syncLocalDBWithServer()
+  .then(_res => {
+    console.log('Sync local DB with server')
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
 window.addEventListener('offline', () => {
   console.log('You are offline')
 })
