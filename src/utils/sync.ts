2908ac7a0716198786d6efaeae77a32380c55f6b
hecsolmay <hecsolmay@gmail.com>
2024-11-15 00:48:38 -0600

    handle new offline methods to sync on server

--- src/utils/sync.ts
+++ src/utils/sync.ts
@@ -1,21 +1,31 @@
 import { $ } from '@lib/dom'
 import { clearAllLocalMovies, getMoviesFromLocalDB } from '@lib/indexDB'
-import { SaveMovieToDB } from '@services/movies'
+import { deleteMovieFromDB, patchMovieWatchedToDB, SaveMovieToDB } from '@services/movies'
 
 export async function syncLocalDBWithServer () {
   const movies = await getMoviesFromLocalDB()
   const $userEmail = $('#user-email')
   const userEmail = $userEmail?.dataset.email ?? ''
-  console.log('INSERTING MOVIES TO SERVER')
-  const postPromises = movies.map(async movie => {
-    return await SaveMovieToDB({ movie, userEmail })
+  console.log('SYNC MOVIES TO SERVER')
+  const postPromises = movies.map(async movieWithAction => {
+    const { action, ...movie } = movieWithAction
+
+    if (action === 'add') {
+      return await SaveMovieToDB({ movie, userEmail })
+    }
+
+    if (action === 'update') {
+      return await patchMovieWatchedToDB(movie.id, userEmail, movie.watched)
+    }
+
+    if (action === 'remove') {
+      return await deleteMovieFromDB({ id: movie.id, userEmail })
+    }
   })
 
-  await Promise.all(postPromises).then(results => {
-    console.log(results)
-  })
+  await Promise.allSettled(postPromises)
 
-  console.log('MOVIES SAVE ON BD CORRECTLY')
+  console.log('MOVIES SYNC ON BD CORRECTLY')
 
   await clearAllLocalMovies()
 }
