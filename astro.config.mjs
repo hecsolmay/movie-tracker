35d7331267c3af3126307c1d2c3daf692cadbf87
hecsolmay <106600467+hecsolmay@users.noreply.github.com>
2024-11-20 16:44:41 -0600

    Update astro.config.mjs to add script tag on service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
     plugins: [
       AstroPWA({
         registerType: 'autoUpdate',
-        injectRegister: 'auto',
+        injectRegister: 'script',
         manifest,
         base: '/',
         workbox: {
