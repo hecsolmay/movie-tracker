6e2aa4de01e55a89d6c138e32338672ebb1370aa
hecsolmay <hecsolmay@gmail.com>
2024-11-21 01:35:59 -0600

    fix AstroPWA integration on astro config to fix service worker not registering

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,43 +14,40 @@ import auth from 'auth-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), auth()],
-  vite: {
-    plugins: [
-      AstroPWA({
-        registerType: 'autoUpdate',
-        injectRegister: 'auto',
-        devOptions: {
-          enabled: true,
-          type: 'module'
-        },
-        manifest,
-        base: '/',
-        workbox: {
-          globDirectory: '.vercel/output/static',
-          globPatterns: ['**/*.{html,js,css,woff,woff2,ttf,eot,ico,ts}'],
-          runtimeCaching: [
-            {
-              urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
-              handler: 'CacheFirst',
-              options: {
-                cacheName: 'images',
-                expiration: {
-                  maxEntries: 100,
-                  maxAgeSeconds: 30 * 24 * 60 * 60
-                }
+  integrations: [
+    tailwind(),
+    db(),
+    auth(),
+    AstroPWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      },
+      manifest,
+      base: '/',
+      workbox: {
+        globPatterns: ['**/*.{css,woff,woff2,ttf,eot,ico}'],
+        runtimeCaching: [
+          {
+            // Imágenes y otros activos estáticos
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-assets',
+              expiration: {
+                maxEntries: 100, // Máximo 100 activos
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 días de vigencia
               }
             }
-          ]
-        }
-      })
-    ],
-    build: {
-      rollupOptions: {
-        external: ['virtual:pwa-register']
+          }
+        ],
+        navigateFallback: null
       }
-    }
-  },
+    })
+  ],
+  vite: {},
   output: 'server',
   adapter: vercel()
 })
